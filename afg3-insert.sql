b5bc70b41059d1cfad0a355915ac14ced8afdeff
Robert Hartings <hello@hartlab.de>
2019-07-13 10:58:40 +0200

    Create afg3-insert.sql

---
+++ afg3-insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO one_col (test) VALUES ('asdf');
+INSERT INTO one_col (test) VALUES ('asdf');
+INSERT INTO one_col (test) VALUES ('asdf');
+INSERT INTO one_col (test) VALUES ('asdf1');
+INSERT INTO one_col (test) VALUES ('asdf2');
+INSERT INTO one_col (test) VALUES ('asdf3');
